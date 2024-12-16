7798a0bd4f4a6caa8f1516c610365397bc6e57b2
tokoba <t.okoba@gmail.com>
2024-12-16 22:44:36 +0900

    added shadcn => BOILERPLATE COMPLETED

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
+import { ModeWatcher } from 'mode-watcher'
 import type { i18n } from 'i18next'
 import getI18nStore from '$lib/i18n'
 import { setContext } from 'svelte'
 import type { Writable } from 'svelte/store'
 import '../app.css'
+
+import NavigationBar from '$lib/components/custom/NavigationBar.svelte'
 
 /* setContext() is a request-scoped store instance to avoid the issue with
  * sveltekit-SSR, when you change languages on the server, it attects server
@@ -15,4 +18,6 @@ export const i18nContext: Writable<i18n> = getI18nStore()
 let { children } = $props()
 </script>
 
+<ModeWatcher />
+<NavigationBar />
 {@render children()}
