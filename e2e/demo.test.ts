6018906525c77334dfdc12a747590d6852bb05d8
tokoba <t.okoba@gmail.com>
2024-12-17 09:55:55 +0900

    test configuration and comments updated

--- e2e/demo.test.ts
+++ e2e/demo.test.ts
@@ -1,10 +1,14 @@
-import { expect, test } from '@playwright/test';
+/* Customize your test conditions for playwright
+ * The default sample is checking the tags on the top page '/'.
+ * https://playwright.dev/
+ */
+import { expect, test } from '@playwright/test'
 
 test('Top page has expected h2', async ({ page }) => {
-	await page.goto('/');
-	await expect(page.locator('h2')).toBeVisible();
-});
+    await page.goto('/')
+    await expect(page.locator('h2')).toBeVisible()
+})
 test('Top page has expected h1', async ({ page }) => {
-	await page.goto('/');
-	await expect(page.locator('h1')).toBeVisible();
-});
+    await page.goto('/')
+    await expect(page.locator('h1')).toBeVisible()
+})
--- src/demo.spec.ts
+++ src/demo.spec.ts
@@ -1,7 +1,10 @@
-import { describe, it, expect } from 'vitest';
+/* Customize your test conditions for vitest
+ * The default sample is just calculating using JavaScript native function.
+ */
+import { describe, it, expect } from 'vitest'
 
 describe('sum test', () => {
-	it('adds 1 + 2 to equal 3', () => {
-		expect(1 + 2).toBe(3);
-	});
-});
+    it('adds 1 + 2 to equal 3', () => {
+        expect(1 + 2).toBe(3)
+    })
+})
