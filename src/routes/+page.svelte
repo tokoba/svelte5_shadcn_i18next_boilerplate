1cf7aa7197648b4689f9812520f12c1a89b7d1ce
tokoba <t.okoba@gmail.com>
2024-12-17 09:58:28 +0900

    update comments

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,14 +4,12 @@ import language from 'i18next'
 import { getContext, onMount } from 'svelte'
 import type { Writable } from 'svelte/store'
 const i18nContext = getContext<Writable<i18n>>('i18n')
-console.log('[page] language:', $i18nContext.language)
-
-// 初期化時に localStorage から言語情報を取得し、設定する
+// console.log('[page] language:', $i18nContext.language)
 </script>
 
-<h1>HOME PAGE</h1>
-<h2>home page</h2>
-<div>
+<h1>This is an example of Svelte5, SvelteKit2, Shadcn-Svelte, Svelte-i18Next.</h1>
+<h2>Customize the configurations as you need.</h2>
+<div class="greeting-various-languages text-5xl text-center">
     {$i18nContext.t('Hello')}
     {$i18nContext.t('World')}
 </div>
